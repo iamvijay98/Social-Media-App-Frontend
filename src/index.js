e796ac1b375ac6cd44d6e4dcff1fac60e42a9a03
iamvijay98 <vijayaragavendran98@gmail.com>
2023-09-06 19:15:45 +0530

    Deployed

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.public_url}>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
